1f06ec8a5348dba9cd0cc5a006b29480db643cea
Dmitriy.Koval <dimonkoval@gmail.com>
2024-05-24 11:10:32 +0300

    #271 added code 401 to response code list in POST/Econews in EcoNewsController

--- core/src/main/java/greencity/controller/EcoNewsController.java
+++ core/src/main/java/greencity/controller/EcoNewsController.java
@@ -67,6 +67,7 @@ public class EcoNewsController {
     @ApiResponses(value = {@ApiResponse(responseCode = "201", description = HttpStatuses.CREATED,
             content = @Content(schema = @Schema(implementation = EcoNewsGenericDto.class))),
         @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
     })
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<EcoNewsGenericDto> save(
